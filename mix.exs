50ed92c16e1044c326ed6cb42bd645ed4d87296d
showwin <showwin_kmc@yahoo.co.jp>
2021-06-17 08:53:57 +0900

    [add] 適当なライブラリを追加してみる

--- mix.exs
+++ mix.exs
@@ -21,8 +21,10 @@ defmodule PracticeElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix, "~> 1.1 or ~> 1.2"},
+      {:phoenix_html, "~> 2.3"},
+      {:cowboy, "~> 1.0", only: [:dev, :test]},
+      {:slime, "~> 0.14"}
     ]
   end
 end
