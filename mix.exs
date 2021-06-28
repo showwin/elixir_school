357d0bd8641c54a45115a06776a3b5359810d214
showwin <showwin_kmc@yahoo.co.jp>
2021-06-29 07:55:15 +0900

    add CLI

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule PracticeElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript(),
     ]
   end
 
@@ -28,5 +29,9 @@ defmodule PracticeElixir.MixProject do
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:png, github: "yuce/png"}  # import Erlang library from git
     ]
+  end
+
+  defp escript do
+    [main_module: PracticeElixir.CLI]
   end
 end
