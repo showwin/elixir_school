29aa8964d6b8ab7ae12102348a90e55be32f981a
showwin <showwin_kmc@yahoo.co.jp>
2021-07-15 09:19:08 +0900

    [add] GenServer

---
+++ lib/gen_server.ex
@@ -0,0 +1,38 @@
+defmodule SimpleQueue do
+  use GenServer
+
+
+  @doc """
+  GenServer.init/1 コールバック
+  """
+  def init(state), do: {:ok, state}
+
+  @doc """
+  GenServer.handle_call/3 コールバック
+  """
+  def handle_call(:dequeue, _from, [value | state]) do
+    {:reply, value, state}
+  end
+
+  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
+
+  def handle_call(:queue, _from, state), do: {:reply, state, state}
+
+  def handle_cast({:enqueue, value}, state) do
+    {:noreply, state ++ [value]}
+  end
+
+  ## クライアント側API
+
+  @doc """
+  キューを開始してリンクする。
+  これはヘルパー関数
+  """
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def queue, do: GenServer.call(__MODULE__, :queue)
+  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
+  def enqueue(value), do: GenServer.cast(__MODULE__, {:enqueue, value})
+end
